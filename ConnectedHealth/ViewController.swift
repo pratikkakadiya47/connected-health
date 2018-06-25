c2a1868154834a5e8ff5f729ec366c176d14bf72
pratik kakadiya <pratikkakadiya@kapils-mac-mini.local>
2018-06-25 11:44:22 +0530

    Initial Commit

---
+++ ConnectedHealth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectedHealth
+//
+//  Created by pratik kakadiya on 25/06/18.
+//  Copyright © 2018 indusa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
